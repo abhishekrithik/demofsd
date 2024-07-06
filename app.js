36a5094bb4a821a6a9558128d67e240c2796507b
abhishekrithik <abhishekrithik2017@gmail.com>
2024-07-06 13:37:32 +0530

    backend done

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 // import express
 const express = require('express');
+const userRouter = require('./routes/userRoutes');
+const requestLogger = require('./utils/logger');
+const unknownEndpoint = require('./utils/Error');
+// const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const productRouter = require('./routes/productRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 // create an express app
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+// app.use(morgan('dev'));
+
+// middleware
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/orders', orderRouter);
+
+app.use(unknownEndpoint);
 
 module.exports = app;
---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+// 1. create a router
+const express = require('express');
+const userRouter = express.Router();
+const userController = require('../controllers/userController');
+const auth = require('../middleware/auth');
+
+// 2. add routes
+// userRouter.get('/', userController.getData);
+userRouter.post('/register', userController.register);
+userRouter.post('/login', userController.login);
+userRouter.get('/logout', userController.logout);
+userRouter.get('/profile', auth.verifyToken, userController.getProfile);
+userRouter.put('/profile', auth.verifyToken, userController.updateProfile);
+userRouter.delete('/profile', auth.verifyToken, userController.deleteProfile);
+
+// 3. export the router
+module.exports = userRouter;
