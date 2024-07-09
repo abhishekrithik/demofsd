443c49d518852cccf315e6a4026973d7ead60365
abhishekrithik <abhishekrithik2017@gmail.com>
2024-07-09 13:32:15 +0530

    backend done

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,12 @@
-
 const express = require('express');
 const productController = require('../controllers/productController');
 const productRouter = express.Router();
 const auth = require('../middleware/auth');
 
-productRouter.post('/', auth.verifyToken, productController.createProduct);
+productRouter.post('/', auth.verifyToken, auth.isAdmin, productController.createProduct);
 productRouter.get('/', auth.verifyToken, productController.getAllProducts);
 productRouter.get('/:id', auth.verifyToken, productController.getProductById);
-productRouter.put('/:id', auth.verifyToken, productController.updateProduct);
-productRouter.delete('/:id', auth.verifyToken, productController.deleteProduct);
+productRouter.put('/:id', auth.verifyToken, auth.isAdmin, productController.updateProduct);
+productRouter.delete('/:id', auth.verifyToken, auth.isAdmin, productController.deleteProduct);
 
 module.exports = productRouter;
